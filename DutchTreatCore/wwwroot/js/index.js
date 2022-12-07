262ead163e19f24e8539608c7a69f2f66e809cb6
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-12-07 09:32:20 -0800

    Adding JavaScript | Bower | CSS | jquery

---
+++ DutchTreatCore/wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+console.log("Hello");
+
+var theForm = $("#theForm");
+theForm.hide();
+var button = $("#buyButton");
+button.on("click", function() {
+    console.log("Buying Item");
+});
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You Clicked on",+$(this).text());
+
+});
+
+    var loginToggle = $("#loginToggle");
+    var popupForm = $(".popup-form");
+
+    loginToggle.on("click", function() {
+        popupForm.slideToggle(1000);
+    });
+});
