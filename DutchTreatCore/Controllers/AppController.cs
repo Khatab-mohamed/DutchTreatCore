21da0cab695099ca73d1e6afa88574533cd1534a
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-12-07 14:15:08 -0800

    Add Mvc

---
+++ DutchTreatCore/Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreatCore.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+    }
+}
